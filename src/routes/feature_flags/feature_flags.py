3d918d5e96671f723bdb885ac9dce1acd3033501
plongzx <pauljorhahaha@gmail.com>
2024-10-19 02:34:40 +0800

    CHORE: revert changes

--- src/routes/feature_flags/feature_flags.py
+++ src/routes/feature_flags/feature_flags.py
@@ -1,6 +1,5 @@
 from fastapi import Request, APIRouter, Body, status
 from src.routes.feature_flags.service import FeatureFlags
-from src.routes.feature_flags.models import CreateFeatureFlag
 
 
 router = APIRouter()
@@ -18,7 +17,7 @@ async def fetch_school_features(school_id: str, query: Request) -> dict:
     return await feature_flag.fetch_school_features(school_id, query_dict)
 
 @router.post("/create", response_model=dict, status_code=status.HTTP_200_OK)
-async def create_school_features(school_features: CreateFeatureFlag) -> dict:
+async def create_school_features(school_features: dict) -> dict:
     return await feature_flag.create_school_features(school_features)
 
 @router.put("/update/{school_id}", response_model=dict, status_code=status.HTTP_200_OK)
